15153db23232d8cd50d205f8323628d5d2a0baf0
Carl <carlwang49@gmail.com>
2024-06-29 06:35:38 +0000

    refactor random_power

--- Methods/random_power.py
+++ Methods/random_power.py
@@ -2,56 +2,51 @@ from EVChargingEnv import EVChargingEnv
 import numpy as np
 from datetime import datetime, timedelta
 from logger_config import configured_logger as logger
+from utils import prepare_ev_request_data, prepare_ev_departure_data, create_result_dir
+
+# TODO: 合併 start_time, start_date, end_time, end_date
+
+# Define the start and end date of the EV request data
+start_date = START_DATE = '2018-07-01'
+end_date = END_DATE = '2018-07-02'
+
+# Define the start and end time of the EV request data
+start_time = START_TIME = datetime(2018, 7, 1)
+end_time = END_TIME = datetime(2018, 7, 2)
+
+num_agents = NUM_AGENTS = 10
+parking_data_path = PARKING_DATA_PATH = '../Dataset/Sim_Parking/ev_parking_data_from_2018-07-01_to_2018-12-31.csv'
 
 
 if __name__ == '__main__':
     """
     Run a simulation of the EV charging environment with random EV request data and random charging power selection.
     """
-    # Set random seed for reproducibility
-    np.random.seed(42)
-    num_agents = 10
+    # Create a directory for storing results of the simulation
+    result_dir = create_result_dir('RandomPower')
     
-    # Generate random EV request data
-    base_date = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-    arrival_times = [base_date + timedelta(hours=int(np.random.uniform(8, 12))) for _ in range(num_agents)]
-    departure_times = [base_date + timedelta(hours=int(np.random.uniform(16, 22))) for _ in range(num_agents)]
-    initial_socs = np.round(np.random.uniform(0.3, 0.5, num_agents), 2)
-    departure_socs = np.round(np.random.uniform(0.5, 0.9, num_agents), 2)
-
-
-    ev_request_data = []
-    for i in range(num_agents):
-        ev_request_data.append({
-            'requestID': i + 1000,  # Generate a unique request ID
-            'arrival_time': arrival_times[i], # Generate a random arrival time
-            'departure_time': departure_times[i], # Generate a random departure time
-            'initial_soc': initial_socs[i], # Generate a random initial SoC
-            'departure_soc': departure_socs[i] # Generate a random departure SoC
-        })
-        
-    # Run the simulation
-    base_date = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0) 
-    current_time = base_date
-    end_time = base_date + timedelta(hours=24)
-    time_interval = 1  # 1 hour
+    # Define the start and end date of the EV request data
+    ev_request_dict = prepare_ev_request_data(parking_data_path, start_date, end_date)
+    ev_departure_dict = prepare_ev_departure_data(parking_data_path, start_date, end_date)
     
     # Set random seed for reproducibility and create an EV charging environment
-    env = EVChargingEnv(num_agents, current_time, end_time)
-
+    env = EVChargingEnv(num_agents, start_time, end_time)
+    current_time = start_time  
     while current_time <= end_time:
         
         count = 0
+        current_requests = ev_request_dict.get(current_time, [])
         # Add EVs that arrived at the current time
-        if current_time not in [ev['arrival_time'] for ev in ev_request_data]:
-            logger.info(f"Current time: {current_time} | No EVs arrived.")
-        else:
-            for ev in ev_request_data:
-                if ev['arrival_time'] == current_time:
-                    env.add_ev(ev['requestID'], ev['arrival_time'], ev['departure_time'], ev['initial_soc'], ev['departure_soc'])
-                    env.current_parking_number += 1
-                    count += 1
-                    logger.info(f"EV {ev['requestID']} arrived at {current_time}")
+        if current_requests:
+            for ev in current_requests:
+                env.add_ev(ev['requestID'], 
+                            ev['arrival_time'], 
+                            ev['departure_time'], 
+                            ev['initial_soc'], 
+                            ev['departure_soc'])
+                
+                env.current_parking_number += 1 # increase the number of EVs in the environment
+                count += 1 # increase the number of EVs that have arrived
 
         # Update the SoC of each connected EV
         for agent_idx, is_parking in enumerate(env.current_parking):
@@ -65,19 +60,19 @@ if __name__ == '__main__':
                 soc = env.get_soc(agent_idx)
                 logger.info(f"EV {ev_data['requestID']} - SoC: {soc}")
         
+        current_departures = ev_departure_dict.get(current_time, [])
         # Remove EVs that departed at the current time
-        if current_time in [ev['departure_time'] for ev in ev_request_data]:
-            for ev in ev_request_data:
-                if ev['departure_time'] == current_time:
-                    agent_idx = np.where([ev['requestID'] == data['requestID'] for data in env.ev_data])[0][0]
-                    env.remove_ev(agent_idx)
-                    env.current_parking_number -= 1
-                    count -= 1
-                    logger.info(f"EV {ev['requestID']} departed at {current_time}")
+        if current_departures:
+            for ev in current_departures:
+                agent_idx = np.where([ev['requestID'] == data['requestID'] for data in env.ev_data])[0][0]
+                env.remove_ev(agent_idx)
+                env.current_parking_number -= 1
+                count -= 1
+                logger.info(f"EV {ev['requestID']} departed at {current_time}")
         
         env.previous_parking_number += count
-        current_time += timedelta(hours=time_interval)
+        current_time += timedelta(hours=1)
 
     # Save the charging records and SoC history to CSV files
-    env.soc_history.to_csv('../Result/Random/soc_history.csv', index=False)
-    env.charging_records.to_csv('../Result/Random/charging_records.csv', index=False)
+    env.soc_history.to_csv(f'../Result/{result_dir}/soc_history.csv', index=False)
+    env.charging_records.to_csv(f'../Result/{result_dir}/charging_records.csv', index=False)
