3bb2752a08418be0e6b221911c074d34c8497ba5
Carl <carlwang49@gmail.com>
2024-07-14 13:58:39 +0000

    set Unschedule

--- Random/main.py
+++ Random/main.py
@@ -4,25 +4,37 @@ from datetime import datetime, timedelta
 from logger_config import configured_logger as logger
 from utils import prepare_ev_request_data, prepare_ev_departure_data, create_result_dir
 import pandas as pd
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
+start_datetime_str = os.getenv('START_DATETIME', '2018-07-01')
+end_datetime_str = os.getenv('END_DATETIME', '2018-10-01')
 
 # Define the start and end datetime of the EV request data
-start_datetime = datetime(2018, 7, 1)
-end_datetime = datetime(2018, 8, 1)
+start_datetime = datetime.strptime(start_datetime_str, '%Y-%m-%d')
+end_datetime = datetime.strptime(end_datetime_str, '%Y-%m-%d')
 
 # Define the start and end date of the EV request data
 start_date = START_DATE = str(start_datetime.date())
 end_date = END_DATE = str(end_datetime.date())
+
+start_date_without_year = START_DATE[5:]  # Assuming the format is 'YYYY-MM-DD'
+end_date_without_year = END_DATE[5:]  # Assuming the format is 'YYYY-MM-DD'
 
 # Define the start and end time of the EV request data
 start_time = START_TIME = start_datetime
 end_time = END_TIME = end_datetime
 
 # Define the number of agents
-num_agents = NUM_AGENTS = 10
+num_agents = NUM_AGENTS = int(os.getenv('NUM_AGENTS'))
 
 # Define the path to the EV request data
 parking_data_path = PARKING_DATA_PATH = f'../Dataset/Sim_Parking/ev_parking_data_from_2018-07-01_to_2018-12-31_{NUM_AGENTS}.csv'
+
+# Define the directory name to save the result
+# dir_name = DIR_NAME = 'GB-MARL-Discrete'
+dir_name = DIR_NAME = 'RandomPower'
 
 
 if __name__ == '__main__':
@@ -30,7 +42,7 @@ if __name__ == '__main__':
     Run a simulation of the EV charging environment with random EV request data and random charging power selection.
     """
     # Create a directory for storing results of the simulation
-    result_dir = create_result_dir('RandomPower')
+    result_dir = create_result_dir(f'{DIR_NAME}_{start_date_without_year}_{end_date_without_year}_{NUM_AGENTS}')
     
     # Define the start and end date of the EV request data
     ev_request_dict = prepare_ev_request_data(parking_data_path, start_date, end_date)
